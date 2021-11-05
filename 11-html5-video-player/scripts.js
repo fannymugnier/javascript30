f0583c727bd1775e5f1f2dd655a0717a6c77c554
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-11-05 17:57:41 +0100

    add updateVideoTime function

--- 11-html5-video-player/scripts.js
+++ 11-html5-video-player/scripts.js
@@ -1,6 +1,8 @@
 const video = document.querySelector('video');
 const playButton = document.querySelector('.toggle');
 const progressBar = document.querySelector('.progress__filled');
+const videoDurationBar = document.querySelector('.progress');
+let isChangingVideoTime = false;
 
 function togglePlay() {
   if (video.paused) {
@@ -17,6 +19,22 @@ function updateProgressBar() {
   progressBar.style.setProperty('flex-basis', `${progression}%`);
 }
 
+function updateVideoTime(e) {
+  video.currentTime = video.duration * (e.offsetX / videoDurationBar.offsetWidth);
+}
+
+// toggle play/pause on click
 playButton.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+
+// progress bar is being filled as video plays
 video.addEventListener('timeupdate', updateProgressBar);
+
+// duration bar clickable to change video current time
+videoDurationBar.addEventListener('click', updateVideoTime);
+videoDurationBar.addEventListener('mousemove', (e) => {
+  if (isChangingVideoTime) updateVideoTime(e);
+});
+videoDurationBar.addEventListener('mousedown', () => isChangingVideoTime = true);
+videoDurationBar.addEventListener('mouseup', () => isChangingVideoTime = false);
+videoDurationBar.addEventListener('mouseout', () => isChangingVideoTime = false);
