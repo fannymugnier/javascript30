670d511bba3856dd4009fb74170ac96ff217d181
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-11-05 19:05:22 +0100

    add skipTime function

--- 11-html5-video-player/scripts.js
+++ 11-html5-video-player/scripts.js
@@ -4,6 +4,7 @@ const progressBar = document.querySelector('.progress__filled');
 const videoDurationBar = document.querySelector('.progress');
 const rangeElements = document.querySelectorAll('.player__slider');
 const timer = document.querySelector('.player__timer');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
 let isChangingVideoTime = false;
 
@@ -37,6 +38,10 @@ function updateParameter() {
   video[this.name] = this.value;
 }
 
+function skipTime() {
+  video.currentTime += parseInt(this.dataset.skip);
+}
+
 // toggle play/pause on click
 playButton.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
@@ -55,3 +60,6 @@ videoDurationBar.addEventListener('mouseout', () => isChangingVideoTime = false)
 
 // handle volume and speed values
 rangeElements.forEach(rangeElement => rangeElement.addEventListener('input', updateParameter));
+
+// handle skip buttons (-10s or +25s)
+skipButtons.forEach(skipButton => skipButton.addEventListener('click', skipTime));
