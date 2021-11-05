a787edd4f276a91bd23c674b17434ce1cdea3eee
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-11-05 18:11:29 +0100

    add updateParameter function

--- 11-html5-video-player/scripts.js
+++ 11-html5-video-player/scripts.js
@@ -2,6 +2,8 @@ const video = document.querySelector('video');
 const playButton = document.querySelector('.toggle');
 const progressBar = document.querySelector('.progress__filled');
 const videoDurationBar = document.querySelector('.progress');
+const rangeElements = document.querySelectorAll('.player__slider');
+
 let isChangingVideoTime = false;
 
 function togglePlay() {
@@ -23,6 +25,10 @@ function updateVideoTime(e) {
   video.currentTime = video.duration * (e.offsetX / videoDurationBar.offsetWidth);
 }
 
+function updateParameter() {
+  video[this.name] = this.value;
+}
+
 // toggle play/pause on click
 playButton.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
@@ -38,3 +44,6 @@ videoDurationBar.addEventListener('mousemove', (e) => {
 videoDurationBar.addEventListener('mousedown', () => isChangingVideoTime = true);
 videoDurationBar.addEventListener('mouseup', () => isChangingVideoTime = false);
 videoDurationBar.addEventListener('mouseout', () => isChangingVideoTime = false);
+
+// handle volume and speed values
+rangeElements.forEach(rangeElement => rangeElement.addEventListener('input', updateParameter));
