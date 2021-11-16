5b633e876857873236da4df6d7d776c71492d8c1
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-11-16 17:19:48 +0100

    add video preview + photo feature

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -3,3 +3,39 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(localMediaStream => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(() => {
+      window.alert('You need to authorize access to camera and reload the page');
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => ctx.drawImage(video, 0, 0, width, height), 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'webcam-photo');
+  link.innerHTML = `<img src="${data}" alt="picture" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
